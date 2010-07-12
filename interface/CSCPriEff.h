7e69bd43f8d2aed5209bbb2a39b6b7b1ffbd5ee7
zhangjin <>
2010-07-12 14:48:02 +0000

    Initial Ver

--- interface/CSCPriEff.h
+++ interface/CSCPriEff.h
@@ -74,10 +74,14 @@ class CSCPriEff : public edm::EDFilter {
    private:
       virtual void beginJob() ;
       virtual bool filter(edm::Event&, const edm::EventSetup&);
-      virtual void HepMCDaughtersTree(HepMC::GenParticle *genPar);
+      virtual void HepMCDaughtersTree(HepMC::GenParticle *);
+      virtual void HepMCParentTreeWZ(HepMC::GenParticle *);
       virtual void HepMCParentTree(HepMC::GenParticle *);
       virtual void SimTrackDaughtersTree(edm::SimVertexContainer &, edm::SimTrackContainer &, vector< vector<int> > &, unsigned int);
+      inline int ParticleType(int pid);
+      virtual Byte_t classification(vector<Int_t> &);
       virtual void endJob() ;
+
       
 //-------------member data------------
    //TTree&TFile
@@ -88,18 +92,11 @@ class CSCPriEff : public edm::EDFilter {
    {
      ULong64_t RUN,EVENT,LS,ORBIT,BX;
    } Info;
-   Double_t minP,maxP,MaxRelpT,MaxDR,tmp_MaxDR2,MaxDR2,maxNormChi2;
    UInt_t minTrackHits;
    //Tracker Muon
    vector<Float_t> *pt,*eta,*phi,*Vertex_x,*Vertex_y,*Vertex_z,*isoR03sumPt,*isoR03emEt,*isoR03hadEt,*isoR03hoEt,*isoR03nJets,*isoR03nTracks,*isoR05sumPt,*isoR05emEt,*isoR05hadEt,*isoR05hoEt,*isoR05nJets,*isoR05nTracks,*isoemVetoEt,*isohadVetoEt,*isohoVetoEt,*normalizedChi2,*TrkRecoChi2,*CaloE_emMax,*CaloE_emS9,*CaloE_emS25,*CaloE_hadMax,*CaloE_hadS9,*Calo_emPos_R,*Calo_emPos_eta,*Calo_emPos_phi,*Calo_hadPos_R,*Calo_hadPos_eta,*Calo_hadPos_phi,*dEdx,*dEdxError,*TrkKink,*GlbKink;
-   vector<Bool_t> *chargeMinus,*isGlobalMu;
-   vector<Int_t> *dEdx_numberOfSaturatedMeasurements,*dEdx_numberOfMeasurements,*whichGenPar,*whichGenId;
-   //Simulated Particles
-   vector<Float_t> *Gen_pt,*Gen_eta,*Gen_phi,*Gen_vx,*Gen_vy,*Gen_vz,*Gen_vt;
-   vector<Int_t> *Gen_pdgId;
-   vector<Byte_t> *Gen_charge;
-   vector<Int_t> DChain, *DChains; //Simulated chains; -1 is at the beginning of each chain; -2is at the beginning of each reco-muons;
-   int particlenum;
+   vector<Bool_t> *chargeMinus,*isGlobalMu,*isTrackerMu;
+   vector<Int_t> *dEdx_numberOfSaturatedMeasurements,*dEdx_numberOfMeasurements;
    //PrimaryVertex
    string PrimaryVerticesTag;
    vector<Float_t> *vx,*vxError,*vy,*vyError,*vz,*vzError;
@@ -109,12 +106,26 @@ class CSCPriEff : public edm::EDFilter {
    string MatchAlgorithm;
    //Tracks
    string tracksTag,dEdxTag;
+   //Simulated Particles
+   vector< vector<Int_t> > SimChains;
    //HepMC Collection
    string HepMCTag;
+   vector< vector<Int_t> > HepMCChains;
+   Float_t HepMCFourVec[4];
+   bool ChainRecord;
+   //Analyzed Decay Chain
+   vector<Float_t> *Gen_pt,*Gen_eta,*Gen_phi,*Gen_vx,*Gen_vy,*Gen_vz,*Gen_vt;
+   vector<Int_t> *Gen_pdgId,DChain;
+   vector<Int_t> *DChains;
+   vector<HepMC::GenParticle *> DChainHep;
+   vector<Long64_t> *MuonType;//all possible chains; -1 is at the beginning of each chain; -2is at the beginning of each reco-muons;
+   vector<Int_t> ParToSimTrk; 
+   vector<HepMC::GenParticle *> ParToHepMC;
+
    //Summarization
    struct Summary
    {
-     ULong64_t Total_Events,Total_RecoMuons,Total_GenMuons,Total_GenElectrons,Total_GenPhotons;
+     ULong64_t Total_Events,Total_TrackerMuons,Total_GlobalMuon,Total_GlobalnotTrackerMuon;
    }Summarization;
    //Errors and Warnings reports
    struct ErrorMsg
@@ -123,8 +134,8 @@ class CSCPriEff : public edm::EDFilter {
      ULong64_t Run,Event;
    }Error;
    //Others
-   static Bool_t FirstEntry;
-   Bool_t LocalRun;
+   static bool FirstEntry;
+   ULong64_t NumMisMatch;
 };
 // ------------ method called once each job just before starting event loop  ------------
 
