b5e9f3a8e920bdc206009d28aaf4a07603e2ef8b
zhangjin <>
2010-08-16 22:13:15 +0000

    multi muon bug fixed

--- interface/CSCPriEff.h
+++ interface/CSCPriEff.h
@@ -1,6 +1,7 @@
 // system include files 
 #include <memory>
 #include <vector>
+#include <map>
 #include <cmath>
 #include <cstring>
 #include <algorithm>
@@ -21,11 +22,14 @@
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/Common/interface/ValueMap.h"
 #include "DataFormats/TrackReco/interface/DeDxData.h"
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHitFwd.h"
 
 //Tracking Particles, Match to simulated tracks
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
 #include "DataFormats/RecoCandidate/interface/TrackAssociation.h"
 #include "SimTracker/TrackAssociation/interface/TrackAssociatorByChi2.h"
+#include "SimTracker/TrackAssociation/interface/TrackAssociatorByHits.h"
 #include "SimTracker/Records/interface/TrackAssociatorRecord.h"
 
 //SimTracks
@@ -70,16 +74,37 @@ class CSCPriEff : public edm::EDFilter {
    public:
       explicit CSCPriEff(const edm::ParameterSet&);
       ~CSCPriEff();
+      enum ParticleType	{LightMeson=1,CharmedMeson=2,ccbarMeson=3,BottomMeson=4,bbarMeson=5,Baryon=6,Lepton=7,W=8,Z=9,Other=10};
 
    private:
-      virtual void beginJob() ;
+      virtual void beginJob();
       virtual bool filter(edm::Event&, const edm::EventSetup&);
-      virtual void HepMCDaughtersTree(HepMC::GenParticle *);
-      virtual void HepMCParentTreeWZ(HepMC::GenParticle *);
       virtual void HepMCParentTree(HepMC::GenParticle *);
-      virtual void SimTrackDaughtersTree(edm::SimVertexContainer &, edm::SimTrackContainer &, vector< vector<int> > &, unsigned int);
-      inline int ParticleType(int pid);
+      virtual void SimTrackDaughtersTree(SimTrack *);
+      inline ParticleType ParticleCata(int);
       virtual Byte_t classification(vector<Int_t> &);
+      const TrackingRecHit* getHitPtr(trackingRecHit_iterator iter) const {return &**iter;}
+      inline Int_t FindSimTrackRecordingPosition( Int_t ParToSimPos ) {
+	Int_t count=0;
+	vector<Bool_t>::const_iterator IsParInHep_iter = IsParInHep->begin();
+	for (; IsParInHep_iter != IsParInHep->end(); IsParInHep_iter++ ) {
+	  if (!*IsParInHep_iter) {
+	    if (count==ParToSimPos) break;
+	    else count++;
+	  }
+	}
+	return IsParInHep_iter-IsParInHep->begin();
+      }
+      inline Int_t FindHepMCRecordingPosition( Int_t ParToHepPos ) {
+	Int_t count=0;
+	vector<Bool_t>::const_iterator IsParInHep_iter = IsParInHep->begin();
+	for (; IsParInHep_iter != IsParInHep->end(); IsParInHep_iter++ )
+	  if (*IsParInHep_iter) {
+	    if (count==ParToHepPos) break;
+	    else count++;
+	  }
+	return IsParInHep_iter-IsParInHep->begin();
+      }
       virtual void endJob() ;
 
       
@@ -92,35 +117,47 @@ class CSCPriEff : public edm::EDFilter {
    {
      ULong64_t RUN,EVENT,LS,ORBIT,BX;
    } Info;
-   UInt_t minTrackHits;
    //Tracker Muon
    vector<Float_t> *pt,*eta,*phi,*Vertex_x,*Vertex_y,*Vertex_z,*isoR03sumPt,*isoR03emEt,*isoR03hadEt,*isoR03hoEt,*isoR03nJets,*isoR03nTracks,*isoR05sumPt,*isoR05emEt,*isoR05hadEt,*isoR05hoEt,*isoR05nJets,*isoR05nTracks,*isoemVetoEt,*isohadVetoEt,*isohoVetoEt,*normalizedChi2,*TrkRecoChi2,*CaloE_emMax,*CaloE_emS9,*CaloE_emS25,*CaloE_hadMax,*CaloE_hadS9,*Calo_emPos_R,*Calo_emPos_eta,*Calo_emPos_phi,*Calo_hadPos_R,*Calo_hadPos_eta,*Calo_hadPos_phi,*dEdx,*dEdxError,*TrkKink,*GlbKink;
    vector<Bool_t> *chargeMinus,*isGlobalMu,*isTrackerMu;
    vector<Int_t> *dEdx_numberOfSaturatedMeasurements,*dEdx_numberOfMeasurements;
+   //Tracks
+   string tracksTag,dEdxTag;
+   vector<UInt_t> *InnerTrack_nValidHits,*InnerTrack_nMissingHits,*InnerTrack_nBadHits;
+   UInt_t minTrackHits;
+   //Muon Chamber Match
+   vector<Float_t> *TrackDist[4],*TrackDistErr[4];
+   vector<UInt_t> *StationMask,*RequiredStationMask;
+   double maxChamberDist,maxChamberDistPull;
    //PrimaryVertex
    string PrimaryVerticesTag;
    vector<Float_t> *vx,*vxError,*vy,*vyError,*vz,*vzError;
    //Tracking particles ( combination of simulated tracks )
-   vector<Float_t> *TrkParticles_pt,*TrkParticles_eta,*TrkParticles_phi,*TrkParticles_chi2;
-   vector<Int_t> *TrkParticles_pdgId;
-   string MatchAlgorithm;
-   //Tracks
-   string tracksTag,dEdxTag;
-   //Simulated Particles
-   vector< vector<Int_t> > SimChains;
-   //HepMC Collection
-   string HepMCTag;
-   vector< vector<Int_t> > HepMCChains;
-   Float_t HepMCFourVec[4];
-   bool ChainRecord;
-   //Analyzed Decay Chain
+   vector<Float_t> *TrkParticles_pt,*TrkParticles_eta,*TrkParticles_phi;
+   vector<Double_t> *MatchQuality;
+   vector<Int_t> *TrkParticles_pdgId,*TrkParticles_charge;
+   //Muon Selectors
+   unsigned int num_Cuts;
+   vector<string> Cuts;
+   vector<Bool_t> * SelectorPassed[30],*MySelector;
+   //To Get the Decay Chains
+      //saved variables
    vector<Float_t> *Gen_pt,*Gen_eta,*Gen_phi,*Gen_vx,*Gen_vy,*Gen_vz,*Gen_vt;
-   vector<Int_t> *Gen_pdgId,DChain;
-   vector<Int_t> *DChains;
-   vector<HepMC::GenParticle *> DChainHep;
+   vector<Int_t> *Gen_pdgId;
    vector<Long64_t> *MuonType;//all possible chains; -1 is at the beginning of each chain; -2is at the beginning of each reco-muons;
-   vector<Int_t> ParToSimTrk; 
-   vector<HepMC::GenParticle *> ParToHepMC;
+   vector<Bool_t> *IsParInHep;
+   vector<Int_t> *DChains;
+      //temparory variables
+   string HepMCTag;
+   bool ChainRecord;
+   vector< vector<Int_t> > SimChains;
+   vector< vector<Int_t> > HepMCChains;
+   vector<Int_t> DChain;
+   vector<SimTrack *> ParToSim,MaskOut;
+   vector<HepMC::GenParticle *> ParToHep;
+   HepMC::GenParticle *HepParticleBegin;
+   map< SimTrack *, vector<SimTrack *> > Daughters;
+   vector<SimVertex> SVC;
 
    //Summarization
    struct Summary
